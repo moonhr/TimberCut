4f13dcad66f3e84fd2165fbb7e4d43f44f8f67e1
moonhr <moonhr153@gmail.com>
2025-01-06 21:03:53 +0900

    shardcn button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Scene from "../three/Scene";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 const Home = () => {
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      <Scene />
+    <div className="flex flex-col  items-center justify-center">
+      <Image src={"/LOGO.png"} alt="Logo" width={100} height={100} />
+      <div style={{ height: "100vh", width: "100vw" }}>
+        <Button>Click</Button>
+        <Scene />
+      </div>
     </div>
   );
 };
